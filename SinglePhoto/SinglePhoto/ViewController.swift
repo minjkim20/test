ca87fe427a5bec1b50b9030e34a1994a7f0c8dcf
minjkim20 <minjkim20@kis.ac>
2020-01-17 11:10:57 +0900

    a

---
+++ SinglePhoto/SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by 김민제 on 16/01/2020.
+//  Copyright © 2020 김민제. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
